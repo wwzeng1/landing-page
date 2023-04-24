26727296f1e7d932499eba45312e1d637d709cb6
= <kevinlu1248@gmail.com>
2023-04-23 20:15:03 -0700

    fixed dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,31 @@ import CallToAction from "./components/CallToAction";
 import { Helmet } from "react-helmet";
 import Navbar from "./components/Navbar";
 import ogimage from "./assets/ogimage.png";
+import { useEffect } from "react";
 
 const theme = extendTheme({
   initialColorMode: "dark",
   useSystemColorMode: false,
 });
 
-export const App = () => (
-  <>
-    <Helmet>
-      <meta property="og:image" content={ogimage} />
-    </Helmet>
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Navbar />
-        <CallToAction />
-      </Box>
-    </ChakraProvider>
-  </>
-);
+export const App = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  useEffect(() => {
+    if (colorMode === "light") {
+      toggleColorMode();
+    }
+  }, []);
+  return (
+    <>
+      <Helmet>
+        <meta property="og:image" content={ogimage} />
+      </Helmet>
+      <ChakraProvider theme={theme}>
+        <Box textAlign="center" fontSize="xl">
+          <Navbar />
+          <CallToAction />
+        </Box>
+      </ChakraProvider>
+    </>
+  );
+};
