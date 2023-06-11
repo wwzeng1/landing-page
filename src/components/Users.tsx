0cfd34fc864ae5c3ca71a4a15cd7e5d0e7068b39
Kevin Lu <kevinlu1248@gmail.com>
2023-06-10 23:24:16 -0700

    added cmu

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -2,12 +2,14 @@ import { HStack, Text, Image, Link } from "@chakra-ui/react";
 import WithFriendsLogo from "../assets/companies/withfriends.svg";
 import StanfordLogo from "../assets/companies/stanford.png";
 import WaterlooLogo from "../assets/companies/waterloo.png";
+import CMULogo from "../assets/companies/cmu.svg";
 
 const data = [
     // {logo: "https://static.wixstatic.com/media/8db8c5_c97c7fc2b2e04d32a03e81812be38786~mv2.png/v1/fill/w_51,h_51,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/8db8c5_c97c7fc2b2e04d32a03e81812be38786~mv2.png", href: "https://plugnplai.com"},
     {logo: "https://assets.website-files.com/627d96126a4d3386dc82ba44/627ef87b95730e9069f4b45e_savvy%20logo%20new.png", href: "https://trysavvy.com"},
     {logo: WithFriendsLogo, href: "https://withfriends.com"},
     {logo: StanfordLogo, href: "https://stanford.edu"},
+    {logo: CMULogo, href: "https://cmu.edu"},
     {logo: WaterlooLogo, href: "https://www.waterloo.ca"},
 ];
 
